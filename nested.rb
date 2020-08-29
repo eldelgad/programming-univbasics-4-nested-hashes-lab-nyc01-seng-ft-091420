29f8fd2cd9ec7e96d32d6d66c3d4b584fc7a7be9
Elias Delgado <evdelgado99@gmail.com>
2020-08-29 23:41:13 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -16,6 +16,8 @@ def hopper
       }
     }
 
+  programmer_hash[:grace_hopper]
+
 end
 
 def alan_kay_is_known_for
@@ -36,6 +38,8 @@ def alan_kay_is_known_for
         :languages => ["C"]
       }
     }
+    
+  programmer_hash[:alan_kay][:known_for]
 
 end
 
@@ -55,19 +59,21 @@ def dennis_ritchies_language
         :languages => ["C"]
       }
     }
+    
+  programmer_hash[:dennis_ritchie][:languages][0]
 
 end
 
 def adding_matz
 # add the following information to the top level of programmer_hash
-# :yukihiro_matsumoto => {
-#   :known_for => "Ruby",
-#   :languages => ["LISP", "C"]
-# }
 # return the entire updated hash
 
 	programmer_hash = 
  		{
+ 		  :yukihiro_matsumoto => {
+        :known_for => "Ruby",
+        :languages => ["LISP", "C"]
+      },
       :grace_hopper => {
         :known_for => "COBOL",
         :languages => ["COBOL", "FORTRAN"]
@@ -81,6 +87,8 @@ def adding_matz
         :languages => ["C"]
       }
     }
+    
+  programmer_hash
 
 end
 
@@ -103,6 +111,10 @@ def changing_alan
         :languages => ["C"]
       }
     }
+    
+  programmer_hash[:alan_kay][:known_for] = "GUI"
+  
+  programmer_hash
 
 end
 
@@ -125,5 +137,9 @@ def adding_to_dennis
         :languages => ["C"]
       }
     }
+    
+  programmer_hash[:dennis_ritchie][:languages] = "Assembly"
+  
+  programmer_hash
 
 end
